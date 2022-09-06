30c6788b769ca8304d97d138474fab27f3f70530
がっちゃん <yuki.dolphin.koga@gmail.com>
2022-09-06 02:53:08 +0000

    refactor: HTTPサーバーの実行をrun関数に切り出し
    #SECTION-055 リファクタリングとテストコード

--- go_todo_app/main.go
+++ go_todo_app/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
 		// memo: ここで http.HandleFunc と http.HandlerFunc を見間違えていて一瞬詰まった
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
+	}
 
-	if err != nil {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to close: %+v", err)
+	}
+
+	return eg.Wait()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
