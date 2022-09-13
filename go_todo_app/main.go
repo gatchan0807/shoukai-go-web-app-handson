94d1d553878b3c6615076af17ba73e7a6e9235bb
がっちゃん <yuki.dolphin.koga@gmail.com>
2022-09-13 01:40:15 +0000

    feat: 関数実行時のポート番号を変更できるようにする
    #SECTION-056 ポート番号を変更できるようにする

--- go_todo_app/main.go
+++ go_todo_app/main.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		// memo: ここで http.HandleFunc と http.HandlerFunc を見間違えていて一瞬詰まった
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
@@ -22,7 +22,7 @@ func run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
@@ -40,7 +40,18 @@ func run(ctx context.Context) error {
 }
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+
+	if err := run(context.Background(), l); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
