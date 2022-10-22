d47062002f28f0e74a86c6e9dae36e9723ee7105
がっちゃん <yuki.dolphin.koga@gmail.com>
2022-10-22 06:28:55 +0000

    feat: HTTP Handlerを定義する処理を追加し、ヘルスチェックをできるようにする
    #SECTION-063 ルーティング定義を分離した「NewMux」を定義する

---
+++ go_todo_app/mux.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func NewMux() http.Handler {
+	// memo: MUXってなに？ => もともと電子回路の文脈でふたつの信号を1つにまとめる装置の意味で使われる用語。　( https://wa3.i-3-i.info/word13228.html )
+	// Goでは "ServerMux is an HTTP request multiplexer." と表現されるように、ルーティングを担当する処理が ServeMux に詰め込まれている
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		// 静的解析のエラーを回避するため明示的に戻り値を捨てている
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	return mux
+}
