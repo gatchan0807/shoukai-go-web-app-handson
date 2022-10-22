6e18c8fe5f6b8a52fa2c8f5fb9952b7f566ab644
がっちゃん <yuki.dolphin.koga@gmail.com>
2022-10-22 06:32:55 +0000

    refactor: run関数からNewMux / NewServerを呼び出す
    #SECTION-064 「run」関数を再度リファクタリングする

--- go_todo_app/main.go
+++ go_todo_app/main.go
@@ -5,19 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/gatchan0807/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -27,34 +20,14 @@ func run(ctx context.Context) error {
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
+
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server{
-		// memo: ここで http.HandleFunc と http.HandlerFunc を見間違えていて一瞬詰まった
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
+	mux := NewMux()
+	s := NewServer(l, mux)
 
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to close: %+v", err)
-	}
-
-	return eg.Wait()
+	return s.Run(ctx)
 }
 
 func main() {
